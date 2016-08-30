763632f3998a805189759d40ff30bdf2b1a8964e
toyship <toyship@mac.com>
2016-08-30 16:40:56 +0900

    Initial Commit

---
+++ Xcode7Tests/Xcode7Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Xcode7Tests.swift
+//  Xcode7Tests
+//
+//  Created by Tachibana Kaoru on 8/30/16.
+//  Copyright © 2016 toyship.org. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode7
+
+class Xcode7Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
